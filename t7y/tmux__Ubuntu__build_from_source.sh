a5531536a81a59ca9a90fa43bd8710ef95d6591b
nokia_du <nokia_du@asus.com>
2018-09-06 18:29:31 +0800

    Modify t7y dockfile to support Container Service

---
+++ t7y/tmux__Ubuntu__build_from_source.sh
@@ -0,0 +1,18 @@
+# Steps to build and install tmux from source on Ubuntu.
+# Takes <25 seconds on EC2 env [even on a low-end config instance].
+VERSION=2.7
+apt-get -y remove tmux
+apt-get -y install wget tar libevent-dev libncurses-dev
+wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
+tar xf tmux-${VERSION}.tar.gz
+rm -f tmux-${VERSION}.tar.gz
+cd tmux-${VERSION}
+./configure
+make
+make install
+cd -
+rm -rf /usr/local/src/tmux-*
+mv tmux-${VERSION} /usr/local/src
+
+## Logout and login to the shell again and run.
+## tmux -V
